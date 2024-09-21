b60f3b1dd4b8610a52d575c1b6a5f14bf02c8855
Rajeshwar Shinde <rajeshwar.shinde@acclivistechnologies.com>
2024-09-21 23:08:21 +0530

    Free RTOS Xen

---
+++ Example/mylib.c
@@ -0,0 +1,8 @@
+int __stack_chk_fail(void){
+	return 0;
+}
+
+int __stack_chk_guard(void){
+
+	return 0;
+}	
---
+++ Example/mylib.h
@@ -0,0 +1,6 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+int __stack_chk_fail(void);
+
+#endif // MYLIB_H
